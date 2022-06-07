2a57ef96d2801ca23b6b18a60e80aa05eb148340
camila <camilafkapp@gmail.com>
2022-06-07 12:18:44 -0300

    starting carousel landing page

--- Front/serviexpress/src/pages/LandingPage.jsx
+++ Front/serviexpress/src/pages/LandingPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import Styles from './LandingPage.module.scss'
+import Carousel from 'react-bootstrap/Carousel'
+import { useState } from 'react'
 
 const LandingPage = () => {
+
+  // function ControlledCarousel() {
+  //   let [index, setIndex] = useState(0);
+  
+  //   let handleSelect = (selectedIndex, e) => {
+  //     setIndex(selectedIndex);
+  //   };
+  // }
+
   return (
     <div className={Styles.landingctn}>
      <h1 className={Styles.title}>ServiExpress</h1>
@@ -9,12 +20,49 @@ const LandingPage = () => {
        <p className={Styles.description}>Find the services you are looking for. You get everything you need in one place.</p>
      </div>
      <h3 className={Styles.subtitle}>Popular professional services</h3>
+  
+  <div className={Styles.carousel}>
+     <Carousel>
+  <Carousel.Item>
+    <img
+      className={Styles.image}
+      src="https://img.freepik.com/foto-gratis/arquitecto-sexo-femenino-sitio-construccion_1157-36105.jpg?w=2000"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Architecture</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
 
-      <div className={Styles.images}>
-       <img src='https://media.istockphoto.com/photos/male-plumber-working-to-fix-leaking-sink-in-home-bathroom-picture-id1205228815?k=20&m=1205228815&s=612x612&w=0&h=dUnbdZnSyrpXCB8aUN-XezuZbnp3jncbJrrJKSidnsM=' alt='plumbing' className={Styles.img} />
-       <img src='' />
-       <img src='' />
-     </div> 
+  <Carousel.Item>
+    <img
+      className={Styles.image}
+      src="https://res.cloudinary.com/mundo/image/upload/c_scale,w_412,h_216,dpr_2/f_jpg,q_auto:good/v1649828915/shutterstock_236457916.jpg?_i=AA"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Plumbing</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className={Styles.image}
+      src="https://img.freepik.com/foto-gratis/carpintero-experimentado-ropa-trabajo-propietario-pequena-empresa-que-trabaja-taller-carpinteria_116124-2124.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Carpentry</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+  </div>
     </div>
   )
 }
