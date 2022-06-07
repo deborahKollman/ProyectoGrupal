0ed07ea9d41526f0d743c9295b0ee1650ecae6f3
Norberto Palma <palmabeto@hotmail.com>
2022-06-06 21:58:58 -0300

    test de error en publications

--- api/src/services/publications.js
+++ api/src/services/publications.js
@@ -2,5 +2,7 @@ const { publications } = require('../database/data.js');
 
 exports.getPublications = () => {
   // Retorna las n publicaciones activas
+  const a = 1;
+  a = 2;
   return publications;
 };
