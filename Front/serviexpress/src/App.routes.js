b1b208c955efd501f80e4b6d33952178c89b6d1e
JalzDeLezZ <j4mes.delez@gmail.com>
2022-06-07 18:23:08 -0500

    [design] login, register, recovery pass

--- Front/serviexpress/src/App.routes.js
+++ Front/serviexpress/src/App.routes.js
@@ -2,10 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import NotFound from './pages/NotFound.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import RecpveryPassword from './pages/RecoveryPassword.jsx';
 
 function App() {
   return (
     <Routes>
+      <Route exact path="/recoverypass/:token" element={<RecpveryPassword/>} />
+      <Route exact path="/register" element={<Register/>} />
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/" element={<LandingPage/>} />
       <Route path='*' element={<NotFound/>} />
