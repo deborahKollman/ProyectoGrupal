e270177345dfb186f0ffb864274ba375765dc7d8
camila <camilafkapp@gmail.com>
2022-06-08 17:55:22 -0300

    fix

--- Front/serviexpress/src/App.routes.js
+++ Front/serviexpress/src/App.routes.js
@@ -2,11 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import NotFound from './pages/NotFound.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
-
 import Detail from './pages/Detail.jsx'
-
 import Register from './pages/Register.jsx';
-import RecpveryPassword from './pages/RecoveryPassword.jsx';
+import RecoveryPassword from './pages/RecoveryPassword.jsx';
 import Seller from './pages/Seller.jsx';
 
 
@@ -14,8 +12,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="/seller" element={<Seller/>} />
-
-      <Route exact path="/recoverypass/:token" element={<RecpveryPassword/>} />
+      <Route exact path="/recoverypass/:token" element={<RecoveryPassword/>} />
       <Route exact path="/register" element={<Register/>} />
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/" element={<LandingPage/>} />
