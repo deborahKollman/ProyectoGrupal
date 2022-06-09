b11803b8195cb34728ff25737ba87835150dca46
Norberto Palma <palmabeto@hotmail.com>
2022-06-09 20:38:01 -0300

    agregado de la pub en cargabase

--- api/src/database/cargabase.js
+++ api/src/database/cargabase.js
@@ -1,5 +1,5 @@
 
-const { Category, Service, User } = require('../../src/database/postgres');
+const { Category, Service, User, Publication } = require('../../src/database/postgres');
 
 const fx = async function() {
     try {
@@ -28,6 +28,58 @@ const fx = async function() {
         electricidad.setServices(instalaciones);
         electricidad.setServices(pinturas);
         electricidad.setServices(asesoramiento);
+
+        const pub1=await Publication.create( 
+            {
+                "id": 1,
+                "date": "2022-06-09T23:25:03.723Z",
+                "state": "Active",
+                "title": "Titulo de la publicacion",
+                "album": [
+                "http://localhost:3001/public/img/pub/pictures-1654817103521.jpeg"
+                ],
+                "detail": "instalaciones sin fallas",
+                "detail_resume": "Sus necesidades son nuestro compromiso",
+                "price": 5000,
+                "userId": 1,
+                "categoryId": 1,
+                "services": []
+                }
+        )
+        const pub2=await Publication.create( 
+        {
+            "id": 2,
+            "date": "2022-06-09T23:25:33.552Z",
+            "state": "Active",
+            "title": "2da publicacion",
+            "album": [
+            "http://localhost:3001/public/img/pub/pictures-1654817133339.jpeg"
+            ],
+            "detail": "instalaciones sin fallas",
+            "detail_resume": "Contrateme",
+            "price": 5000,
+            "userId": 1,
+            "categoryId": 2,
+            "services": []
+            },
+        )
+        const pub3=await Publication.create( 
+            {
+                "id": 3,
+                "date": "2022-06-09T23:28:02.650Z",
+                "state": "Active",
+                "title": "3era publicacion",
+                "album": [
+                "http://localhost:3001/public/img/pub/pictures-1654817282324.jpeg"
+                ],
+                "detail": "Lo mas",
+                "detail_resume": "Contrateme",
+                "price": 200,
+                "userId": 1,
+                "categoryId": 3,
+                "services": []
+                }
+        )
     }
     catch (error) {
         console.log(error)
