6adb5a3ceb1caeff7e114dda63adbeffa1a6c68c
camila <camilafkapp@gmail.com>
2022-06-09 00:34:45 -0300

    adding not found

--- Front/serviexpress/src/App.routes.js
+++ Front/serviexpress/src/App.routes.js
@@ -4,7 +4,7 @@ import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
 import Detail from './pages/Detail.jsx'
 import Register from './pages/Register.jsx';
-import RecpveryPassword from './pages/RecpveryPassword.jsx';
+import RecoveryPassword from './pages/RecoveryPassword.jsx';
 import Seller from './pages/Seller.jsx';
 import ProfileUser from './pages/ProfileUser.jsx';
 import CreateService from './pages/CreateService.jsx';
@@ -16,7 +16,7 @@ function App() {
       <Route exact path="/seller/add-service" element={<CreateService/>} />
       <Route exact path="/seller" element={<Seller/>} />
       <Route exact path="/user" element={<ProfileUser/>} />
-      <Route exact path="/recoverypass/:token" element={<RecpveryPassword/>} />
+      <Route exact path="/recoverypass/:token" element={<RecoveryPassword/>} />
       <Route exact path="/register" element={<Register/>} />
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/" element={<LandingPage/>} />
