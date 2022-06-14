4c7661f0b85220ced522fca1dece9a4406c491c4
JalzDeLezZ <j4mes.delez@gmail.com>
2022-06-14 16:44:22 -0500

    VERSION 18

--- Front/admin/src/components/Featured.jsx
+++ Front/admin/src/components/Featured.jsx
@@ -1,5 +1,8 @@
 import "./styles/Featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CircularStatic from './sub components/progressbar'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
   return (
@@ -10,7 +13,35 @@ const Featured = () => {
       </div>
       <div className="bottom">
         <div className="featuredChart">
-          
+          <CircularStatic value={70} text="ASDF"/>
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included.
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small"/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
