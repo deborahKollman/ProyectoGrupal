1b1494c71315eb5546b9dec8810726f12011296c
camila <camilafkapp@gmail.com>
2022-06-14 15:56:50 -0300

    continue filter by categories

--- Front/serviexpress/src/redux/action.js
+++ Front/serviexpress/src/redux/action.js
@@ -98,7 +98,7 @@ export const jalz_getAllCategories = () => {
       const { data } = await axios.get(`http://127.0.0.1:3001/categories`);
       dispatch({
         type: "JALZ_GET_CATEGORIES",
-        payload: data,
+        payload: data.map,
       });
     } catch (error) {
       return error.message;
@@ -125,8 +125,8 @@ export function filterCategories(payload) {
 export const getAllCategories = () => {
   return async (dispatch) => {
     try {
-      const json = axios(`${URL}/categories`);
-      return dispatch({ type: "GET_CATEGORIES", payload: json.data });
+      const json = axios.get(`${URL}/categories`);
+      return dispatch({ type: "GET_CATEGORIES", payload: json.data.map((el) => el.name) });
     } catch (error) {
       console.log(error);
     }
