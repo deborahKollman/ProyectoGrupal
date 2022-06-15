094312b8e00002b5cb388f2715b1bc870138025b
Khatanna <carlos65357@gmail.com>
2022-06-15 10:11:41 -0400

    fixed delete favorite

--- api/src/services/users.js
+++ api/src/services/users.js
@@ -1,9 +1,7 @@
-
 const { users } = require('../database/data.js');
 const { User, Favorite, Publication } = require('../database/postgres.js');
 const bcrypt = require('bcryptjs');
-const fs = require('fs')
-
+const fs = require('fs');
 
 /* exports.checkUser = async(usr, password) => {
   // Chequea si el usuario existe y si la clave es correcta
@@ -19,15 +17,15 @@ const fs = require('fs')
   return {err_msg:'Usuario no encontrado'}
 }; */
 
-exports.checkUser = async(usr) => {
+exports.checkUser = async (usr) => {
   // Chequea si el usuario existe
-  const user = await User.findOne({where:{email:usr}})
+  const user = await User.findOne({ where: { email: usr } });
 
-  if(user){
-    return {message: 1}
+  if (user) {
+    return { message: 1 };
   } else {
-    return {message: 0}
-  };
+    return { message: 0 };
+  }
 };
 
 exports.getAllUsers = async ({ page, offset, limit }) => {
@@ -57,14 +55,14 @@ exports.getAllUsers = async ({ page, offset, limit }) => {
   };
 };
 exports.createUser = async (newUser) => {
-  var hash = bcrypt.hashSync(newUser.password, 10);
-  newUser={...newUser,password:hash};
-  const [user,created] = await User.findOrCreate({
-    where:{email:newUser.email},
-    defaults:{...newUser}
-  })
+  const hash = bcrypt.hashSync(newUser.password, 10);
+  newUser = { ...newUser, password: hash };
+  const [user, created] = await User.findOrCreate({
+    where: { email: newUser.email },
+    defaults: { ...newUser }
+  });
   user.createFavorite();
-  return [user,created];
+  return [user, created];
 };
 exports.registerUser = (usr, password) => {
   // retorna un obj con el id de usuario si se registra correctamente o un obj con el motivo del error
@@ -72,140 +70,151 @@ exports.registerUser = (usr, password) => {
   // Cambiar por el nuevo id registrado
   const idRegistered = 8;
 
-  const usrFound = User.findOne({where:{email:usr}});
+  const usrFound = User.findOne({ where: { email: usr } });
   if (usrFound) return { err_msg: 'Email is already registered' };
   else return { idRegistered };
 };
 
-exports.updateUser = async(id,changes) => {
-  const user = await User.findByPk(id)
-  if(user){
-    if(user.dataValues.avatar_image){
+exports.updateUser = async (id, changes) => {
+  const user = await User.findByPk(id);
+  if (user) {
+    if (user.dataValues.avatar_image) {
       fs.unlinkSync(user.dataValues.avatar_image);
     }
-    await User.update({...changes},{where:{id}})
-    return {message:'User updated successfully'}
+    await User.update({ ...changes }, { where: { id } });
+    return { message: 'User updated successfully' };
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.recoverUserPwd = async(usr) => {
-  //const usrFound = users.find((u) => u.usr_email === usr);
+exports.recoverUserPwd = async (usr) => {
+  // const usrFound = users.find((u) => u.usr_email === usr);
   const usrFound = await User.findOne({
-    where:{email:usr}
-  })
+    where: { email: usr }
+  });
 
   if (!usrFound) return { err_msg: 'Email is not registered' };
   else return { message: 'The recovery email was send' };
 };
 
-exports.updatePassword = async(email,password) => {
- 
+exports.updatePassword = async (email, password) => {
   const usrFound = await User.findOne({
-    where:{email:email}
-  })
-  
-  if (usrFound){ 
-    var hash = bcrypt.hashSync(password, 10);
-    if(bcrypt.compareSync(password,usrFound.dataValues.password)){
-      return {err_msg:'Password cannot be previous password'}
+    where: { email }
+  });
+
+  if (usrFound) {
+    const hash = bcrypt.hashSync(password, 10);
+    if (bcrypt.compareSync(password, usrFound.dataValues.password)) {
+      return { err_msg: 'Password cannot be previous password' };
     }
 
-    User.update({password:hash},{where:{email}})
+    User.update({ password: hash }, { where: { email } });
     return { message: 'Password has been changed' };
+  } else return { err_msg: 'Email is not registered' };
+};
 
-  }else return { err_msg: 'Email is not registered' };
-}
-
-exports.deleteUser = async(id) => {
+exports.deleteUser = async (id) => {
   const user = await User.findByPk(id);
-  if(user){
-    if(user.dataValues.avatar_image){
+  if (user) {
+    if (user.dataValues.avatar_image) {
       fs.unlinkSync(user.dataValues.avatar_image);
     }
-    await Favorite.destroy({where:{userId:id}});
-    await User.destroy({where:{id}})
-    return {message:'User deleted successfully'}
+    await Favorite.destroy({ where: { userId: id } });
+    await User.destroy({ where: { id } });
+    return { message: 'User deleted successfully' };
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.getSellerComments = async(id,count=0) => {
+exports.getSellerComments = async (id, count = 0) => {
   const user = await User.findByPk(id);
-  if(user){
-    const comments = user.dataValues.seller_opinions.slice(-count)
+  if (user) {
+    const comments = user.dataValues.seller_opinions.slice(-count);
     return comments;
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.getBuyerComments = async(id,count=0) => {
+exports.getBuyerComments = async (id, count = 0) => {
   const user = await User.findByPk(id);
-  if(user){
-    const comments = user.dataValues.buyer_opinions.slice(-count)
+  if (user) {
+    const comments = user.dataValues.buyer_opinions.slice(-count);
     return comments;
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.addSellerComment = async(id,rating,comment,commenter) => {
+exports.addSellerComment = async (id, rating, comment, commenter) => {
   const user = await User.findByPk(id);
-  if(user){
-    const opinion={commenter,comment,rating};
-    var comm=user.dataValues.seller_opinions;
-    var rep=user.dataValues.seller_reputation;
-    var rep=(rep*comm.length+rating)/(comm.length+1);
+  if (user) {
+    const opinion = { commenter, comment, rating };
+    const comm = user.dataValues.seller_opinions;
+    var rep = user.dataValues.seller_reputation;
+    var rep = (rep * comm.length + rating) / (comm.length + 1);
     comm.push(opinion);
-    await User.update({seller_reputation:rep,seller_opinions:comm},{where:{id}})
-    return {message:'Comment added successfully'}
+    await User.update(
+      { seller_reputation: rep, seller_opinions: comm },
+      { where: { id } }
+    );
+    return { message: 'Comment added successfully' };
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.addBuyerComment = async(id,rating,comment,commenter,buyer_avatar) => {
+exports.addBuyerComment = async (
+  id,
+  rating,
+  comment,
+  commenter,
+  buyer_avatar
+) => {
   const user = await User.findByPk(id);
-  if(user){
-    const opinion={commenter,comment,rating,buyer_avatar};
-    var comm=user.dataValues.buyer_opinions
-    var rep=user.dataValues.buyer_reputation;
-    var rep=(rep*comm.length+rating)/(comm.length+1);
+  if (user) {
+    const opinion = { commenter, comment, rating, buyer_avatar };
+    const comm = user.dataValues.buyer_opinions;
+    var rep = user.dataValues.buyer_reputation;
+    var rep = (rep * comm.length + rating) / (comm.length + 1);
     comm.push(opinion);
-    await User.update({buyer_reputation:rep, buyer_opinions:comm},{where:{id}})
-    return {message:'Comment added successfully'}
+    await User.update(
+      { buyer_reputation: rep, buyer_opinions: comm },
+      { where: { id } }
+    );
+    return { message: 'Comment added successfully' };
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'User not found' };
+};
 
-exports.getFavorites = async(id) => {
-  
-  const favorites = await Favorite.findOne({
-    where:{userId:id},
-    include: {
-      model:Publication,
-      through:{
-        attributes:[]
-      }}
+exports.getFavorites = async (id) => {
+  const favorites = await Favorite.findAll({
+    where: { userId: id },
+    include: [{ model: Publication }]
   });
-  if(!favorites){
-    return {err_msg:'User not found'}
+  if (!favorites.length) {
+    return { err_msg: 'Favorites of user not found' };
   }
-  return favorites
-}
+  return favorites;
+};
 
-exports.addFavorite = async(id, publication) => {
-  const fav = await Favorite.findOne({where:{userId:id}});
-  if(fav){
-    fav.addPublication(publication);
-    return {message:'Publication added to Favorites'}
-  }
-  return {err_msg:'User not found'}
-}
+exports.addFavorite = async (id, publication) => {
+  const user = await User.findByPk(id);
+  const favorite = await Favorite.create();
 
-exports.removeFavorite = async(id, publication) => {
-  const fav = await Favorite.findOne({where:{userId:id}});
-  if(fav){
-    fav.removePublication(publication);
-    return {message:'Publication removed from Favorites'}
+  await favorite.setUser(user);
+  if (favorite) {
+    await favorite.addPublication(publication);
+    return { message: 'Publication added to Favorites' };
   }
-  return {err_msg:'User not found'}
-}
+  return { err_msg: 'Favorite already exists' };
+};
+
+exports.removeFavorite = async (id, publication) => {
+  const fav = await Favorite.findOne({
+    where: { userId: id },
+    include: [{ model: Publication, where: { id: publication } }]
+  });
+  if (fav) {
+    await fav.destroy();
+    return { message: 'Publication removed from Favorites' };
+  }
+  return { err_msg: 'Favorite of user not found' };
+};
