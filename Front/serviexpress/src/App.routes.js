39704e4493522fea0d70e41e7f8971f6a7749c8b
camila <camilafkapp@gmail.com>
2022-06-17 15:46:01 -0300

    continue orders page

--- Front/serviexpress/src/App.routes.js
+++ Front/serviexpress/src/App.routes.js
@@ -13,10 +13,7 @@ import Orders from "./pages/Orders";
 import Home from "./pages/Home";
 import CreateService from "./pages/CreateService";
 import { useSelector } from "react-redux";
-
-import Edit from "./pages/edit.jsx";
 import Payment from "./components/Payment.jsx";
-
 import MyPublications from "./pages/MyPublications";
 import DetailEdit from "./pages/DetailEdit"
 
