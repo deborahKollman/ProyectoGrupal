eeb7d01f5ed3f2b02b7e4f1f91bd9e3d730d4137
Debo <deborahkollman@gmail.com>
2022-06-22 14:57:51 -0300

    cors changes

--- api/server.js
+++ api/server.js
@@ -18,29 +18,14 @@ server.use(morgan('dev'));
 server.use(express.json());
 // server.use(cors());
 
-const whitelist = ['http://localhost:3000', 'http://localhost:4000', 'https://serviexpress-client.vercel.app']
-const corsOption = {
-  credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin, X-Requested-With, Content-Type, Accept, Authorization'],
-    origin: function (origin, callback){
-      if(whitelist.indexOf(origin)!==-1){
-        callback(null, true)
-      }
-      else{
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-}
-
 server.use(
-  cors(corsOption)
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:4000', 'https://serviexpress-client.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+  })
 );
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   next();
-// });
+
 server.use(express.static('public'));
  server.use(
    cookieSession({
@@ -62,7 +47,12 @@ server.use(
 server.use(passport.initialize());
 server.use(passport.session());
 server.use(passport.authenticate('session'));
-
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 // root of routes
 server.use('/', router);
