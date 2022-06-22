b475924857824db2969f039ebf8a50f3cc39d421
Debo <deborahkollman@gmail.com>
2022-06-22 15:00:45 -0300

    cors changes

--- api/server.js
+++ api/server.js
@@ -48,6 +48,7 @@ server.use(passport.initialize());
 server.use(passport.session());
 server.use(passport.authenticate('session'));
 server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
