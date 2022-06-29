4894e2661399f074b43bb278b55b1d502b2722cc
camila <camilafkapp@gmail.com>
2022-06-29 02:18:33 -0300

    improving review

--- Front/serviexpress/src/pages/Review.jsx
+++ Front/serviexpress/src/pages/Review.jsx
@@ -3,37 +3,70 @@ import BurgerButton from '../components/NavBar/NavBar'
 import FooterBar from '../components/FooterBar/FooterBar'
 import Styles from './styles/Review.module.scss'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import {FaStar} from 'react-icons/fa'
 import { useState } from 'react'
+import { postReview } from '../redux/action'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import swal from 'sweetalert'
 
 const Review = () => {
-  
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [input, setInput] = useState({
+    review: '',
+    rating: ''
+  })
+
   const [rating, setRating] = useState()
   const [hover, setHover] = useState()
 
-
-  const handleClick = (value) => {
-       setRating(value)
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name] : e.target.value
+    })
   }
-
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(postReview(input))
+    swal({
+      title: "Review Submitted",
+      text: "Thank you for your review!",
+      icon: "success",
+    })
+    setInput({
+      review: '',
+      rating: ''
+    })
+    navigate('/home')
+  }
 
   return (
     <div className={Styles.container} >
         <BurgerButton />
 
         <div className={Styles.form}>
-        <h4>Give your review!</h4>
-        <Form.Control as="textarea" rows={10} />
+        <h4>Give us your review!</h4>
+
+        <Form onSubmit={handleSubmit}>
+         <Form.Control as="textarea" rows={10} placeholder="Write something..." onChange={(e) => handleChange(e)}/>
+
 
         {[...Array(5)].map((star, i) => {
           const ratingValue = i + 1;
           return (
-          <label>
+            <label>
             <input 
             type='radio'
             name='rating'
             value={ratingValue}
             onClick={() => setRating(ratingValue)}
+            onChange={(e) => handleChange(e)}
             />
             <FaStar
             className={Styles.star}
@@ -45,10 +78,14 @@ const Review = () => {
           </label>
           )
         })
-        }
+      }
 
-        <h5>The rating is {rating}</h5>
+           <h5>Rating selected: {rating}</h5>
        
+            <Button variant="primary" type="submit" className={Styles.btn}>
+              Submit
+            </Button>
+           </Form>
         </div>
         
         <FooterBar />
