0da281e6514a9ef714910767b4da46d296c8413e
camila <camilafkapp@gmail.com>
2022-06-29 01:58:50 -0300

    adding reviews

--- Front/serviexpress/src/pages/Review.jsx
+++ Front/serviexpress/src/pages/Review.jsx
@@ -2,11 +2,55 @@ import React from 'react'
 import BurgerButton from '../components/NavBar/NavBar'
 import FooterBar from '../components/FooterBar/FooterBar'
 import Styles from './styles/Review.module.scss'
+import Form from 'react-bootstrap/Form'
+import {FaStar} from 'react-icons/fa'
+import { useState } from 'react'
 
 const Review = () => {
+  
+  const [rating, setRating] = useState()
+  const [hover, setHover] = useState()
+
+
+  const handleClick = (value) => {
+       setRating(value)
+  }
+
+
   return (
     <div className={Styles.container} >
         <BurgerButton />
+
+        <div className={Styles.form}>
+        <h4>Give your review!</h4>
+        <Form.Control as="textarea" rows={10} />
+
+        {[...Array(5)].map((star, i) => {
+          const ratingValue = i + 1;
+          return (
+          <label>
+            <input 
+            type='radio'
+            name='rating'
+            value={ratingValue}
+            onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+            className={Styles.star}
+            color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+            onMouseEnter={() => setHover(ratingValue)}
+            onMouseLeave={() => setHover(null)}
+            size={50}
+            />
+          </label>
+          )
+        })
+        }
+
+        <h5>The rating is {rating}</h5>
+       
+        </div>
+        
         <FooterBar />
     </div>
   )
